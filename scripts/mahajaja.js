408f21be926c08e5f1ee596c27efc11ec6e26904
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-10-09 20:10:35 -0400

    correcting LS instances and addin promise

--- scripts/mahajaja.js
+++ scripts/mahajaja.js
@@ -1,9 +1,9 @@
 const dataURL = "../JSONdata/A2-JSON.json"
 const categoriesyArray = [];
 const animalsArray = [];
-let responseData = null;
 
 $(function() {
+    //call to ajax
 
     $.ajax({
         type: "GET",
@@ -11,18 +11,21 @@ $(function() {
         dataType: "json",
         success: function (response) {
             // responseData = response;
-            setPersonalData(response);
-            setCategories(response);
-            setAnimals(response);
+            Promise.all([
+                setPersonalData(response),setCategories(response),setAnimals(response)
+            ]).then(()=>{
+                LoadHeader();
+                LoadFooter();
+            });
         }
     });
 });
 
 const setPersonalData = (data)=>{
     const { A2Personal } = data; // destructuring data from json
-    localStorage.setItem("Firstname",A2Personal.FirstName);
+    localStorage.setItem("FirstName",A2Personal.FirstName);
     localStorage.setItem("LastName",A2Personal.LastName);
-    localStorage.setItem("StudentId",A2Personal.StudentID);
+    localStorage.setItem("StudentID",A2Personal.StudentID);
     localStorage.setItem("UserName",A2Personal.UserName);
     localStorage.setItem("Program",A2Personal.Program);
     localStorage.setItem("HomeCountry",A2Personal.HomeCountry);
@@ -61,3 +64,29 @@ class AnimalDetail{
         this.photoDepiction = photoDepiction;
     }
 };
+
+const  LoadHeader = () => {
+    $("#main").html(
+        `
+            <h5>Assignment-2</h5>
+            <h5>Name: ${localStorage.getItem('FirstName')} ${localStorage.getItem('LastName')} </h5>
+            <h5>StudentID: ${localStorage.getItem('StudentID')}</h5>
+            <h5>Program: ${localStorage.getItem('Program')}</h5>
+            <h5>StudentID: ${localStorage.getItem('UserName')}</h5>
+        `
+    );
+    $("#main").addClass("textCenter");
+}
+
+const  LoadFooter = () => {
+    $("#footer").html(
+        `
+            <hr>
+            <h5>My Sheridan Program: ${localStorage.getItem('Program')} </h5>
+            <h5>StudentID: ${localStorage.getItem('StudentID')}</h5>
+            <h5>Program: ${localStorage.getItem('Program')}</h5>
+            <h5>My Home Country: ${localStorage.getItem('HomeCountry')}</h5>
+        `
+    );
+    $("#footer").addClass("textCenter");
+}
